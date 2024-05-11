8be4def43f7133eea3f31527f565ddf2e6b5b5cd
mariiakoval <alexwizard223@>
2024-05-11 18:06:46 +0300

    Final version

--- folders_synchronization.py
+++ folders_synchronization.py
@@ -1,1 +1,75 @@
-#test
+import argparse
+import logging
+import os
+import shutil
+from filecmp import dircmp
+import schedule
+import time
+
+
+def sync_folders(source, replica):
+    """
+    Synchronize two folders including subfolders
+    """
+    try:
+        comparison = dircmp(source, replica)
+
+        # Copy files from source to replica and overwrite if already exist
+        for file in comparison.left_only + comparison.diff_files:
+            src_path = os.path.join(source, file)
+            rpl_path = os.path.join(replica, file)
+
+            if os.path.isdir(src_path):
+                shutil.copytree(src_path, rpl_path)  # Copy entire directory tree
+                logging.info(f"Copied directory from {src_path} to {rpl_path}")
+            else:
+                shutil.copy2(src_path, rpl_path)  # Copy files and preserve metadata
+                logging.info(f"Copied file from {src_path} to {rpl_path}")
+
+        # Recursively sync subdirectories
+        for sub_dir in comparison.common_dirs:
+            sync_folders(os.path.join(source, sub_dir), os.path.join(replica, sub_dir))
+
+        # Remove files and folders not present in source
+        for file in comparison.right_only:
+            rpl_path = os.path.join(replica, file)
+            if os.path.isdir(rpl_path):
+                shutil.rmtree(rpl_path)  # Remove directory tree
+                logging.info(f"Removed directory {rpl_path}")
+            else:
+                os.remove(rpl_path)  # Remove file
+                logging.info(f"Removed file {rpl_path}")
+    except Exception as e:
+        logging.error(f"Error synchronizing {src_path} to {rpl_path}: {str(e)}")
+
+
+def main(source, replica, interval, log_path):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        # Delete following line if milliseconds are needed in function output
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        handlers=[
+                            logging.FileHandler(log_path),
+                            logging.StreamHandler()
+                        ])
+
+    # Initial synchronization and scheduling periodic sync
+    logging.info("Starting initial synchronization.")
+    schedule.every(interval).seconds.do(sync_folders, source, replica)  #Default interval is 3600 secs
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)  # Prevents the loop from consuming excessive CPU resources
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Synchronize two folders.")
+    parser.add_argument("source", help="Source directory path")
+    parser.add_argument("replica", help="Replica directory path")
+    parser.add_argument("--interval", type=int, default=3600,
+                        help="Time interval in seconds for synchronization. Default value is 3600 seconds")
+    parser.add_argument("--log_path", default="sync.log", help="Path to the log file")
+
+    args = parser.parse_args()
+
+    main(args.source, args.replica, args.interval, args.log_path)
